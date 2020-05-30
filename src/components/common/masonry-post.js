978af4cfd85f807e7d95ec973b9662c394b0417f
Taro <taro.6612.taro@gmail.com>
2020-05-30 16:36:02 +0900

    trending posts

--- src/components/common/masonry-post.js
+++ src/components/common/masonry-post.js
@@ -1,5 +1,17 @@
 import React from "react"
 
 export default function MansoryPost({ post, tagsOnTop }) {
-    return <div>F**K U</div>
+    const style = { backgroundImage: `url("${require(`../../assets/images/${post.image}`)}")` };
+    return (
+        <a className="mansory-post overlay" style={style} href={post.link}>
+            <div className="image-text">
+                <div>
+                    <h2 className="image-title">{post.title}</h2>
+
+                </div>
+
+            </div>
+
+        </a >
+    )
 }
