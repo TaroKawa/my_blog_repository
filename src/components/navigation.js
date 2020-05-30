e7f935b449e4d31fc841d498fa838efdd20a91af
Taro <taro.6612.taro@gmail.com>
2020-05-30 12:12:57 +0900

    setState

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Avatar } from "antd"
 
@@ -19,6 +19,7 @@ const navLinks = [
 ]
 
 export default function Navigation({ user }) {
+    const [menuActive, setMenuActive] = useState(false)
     return (
         <nav className="site-navigation">
             <span className="nume-title">My Awwsome Blog</span>
@@ -33,8 +34,9 @@ export default function Navigation({ user }) {
                 <span>
 
                     <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" size={32} />
-                    <span className="menu-avatar-name">{`${user.firstName} ${user.lastName}`}</span>
+                    <span className="menu-avatar">{`${user.firstName} ${user.lastName}`}</span>
                 </span>
+                <i className="ionicons icon ion-ios-menu" onClick={() => setMenuActive(!menuActive)} />
             </div>
         </nav>);
 }
